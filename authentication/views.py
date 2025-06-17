9d64f3c7468075d070ffe057ee42ad862bacfda3
kishart <amoralyse21@gmail.com>
2025-06-17 21:50:58 +0800

    first commit

---
+++ authentication/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+# Create your views here.
+def home(request):
+      return render(request, "authentication/index.html")
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        name = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password != confirm_password:
+            messages.error(request, "Passwords do not match!")
+            return render(request, "authentication/signup.html")
+
+        myuser = User.objects.create_user(username=username, email=email, password=password)
+        myuser.first_name = name
+        myuser.save()
+
+        messages.success(request, "Your account has been created successfully!")
+        return redirect("signin")
+    else:
+        return render(request, "authentication/signup.html")
+
+def signin(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            name = user.first_name
+            return render(request, "authentication/index.html", {'name':name})
+        else:
+            messages.error(request, "Bad Credentials!")
+            return redirect('home')
+    return render(request, "authentication/signin.html")
+
+def signout(request):
+    logout(request)
+    messages.success(request, "Logged out successfully!")
+    return redirect('home')
